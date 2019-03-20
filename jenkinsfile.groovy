89b4dcd626d71d9d5c84116319e0debf04f82ffd
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-20 16:43:44 -0700

    jenkins initial file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,19 @@
+node {
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give IP to build a site', name: 'IP', trim: true)])])
+ 
+    stages ("Install git") {
+        sh 'ssh ec2-user@IP     sudo yum install git -y  python-pip -y'
+     }
+     stage("Clone a repo") {
+         git 'https://github.com/aliyaaloca/flask-examples.git'
+     }
+     stage("Copy files") {
+        sh "scp *ec2-user@IP:/tmp/" 
+     }
+     stage("Install requirements") {
+         sh "ssh ec2-user@IP     sudo pip install -r /tmp/requirements.txt"
+     }
+     stage("Run App") {
+         sh "ssh ec2-user@IP   python /tmp/01-hello-world/hello.py"
+     }
+}
